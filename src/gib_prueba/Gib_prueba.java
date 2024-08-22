1b0ce1f21b259b9e5ac26f9a29c1acbf600f45ea
Holagib2022 <gastonfrias1995@gmail.com>
2024-08-22 20:14:39 -0300

    Initial commit

---
+++ src/gib_prueba/Gib_prueba.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gib_prueba;
+
+/**
+ *
+ * @author gasto
+ */
+public class Gib_prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ src/gib_prueba/paketedeclases/River.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gib_prueba.paketedeclases;
+
+/**
+ *
+ * @author gasto
+ */
+public class River {
+    private int hinchas;
+    private String ubicacion;
+
+    public River(int hinchas, String ubicacion) {
+        this.hinchas = hinchas;
+        this.ubicacion = ubicacion;
+    }
+    
+    
+
+    public int getHinchas() {
+        return hinchas;
+    }
+
+    public void setHinchas(int hinchas) {
+        this.hinchas = hinchas;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+    
+}
